9f8598e7ccd297728bd8e2ce52b2979adc161981
Eric Dennison <ericd@netdenizen.com>
2017-07-26 14:44:24 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,8 +4,6 @@ set -e # exit with nonzero exit code if anything fails
 # create the tests folder
 mkdir tests
 cd tests
-git config user.email "you@example.com"
-git config user.name "Your Name"
 git init
 git pull https://github.com/BrythonServer/ggame.git
 git pull https://${GH_REPO}
