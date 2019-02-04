a9fb5b83cd11ad5d5a9a85e94ed8ee3b1ed12a1d
sth23 <47009471+sth23@users.noreply.github.com>
2019-02-04 14:32:48 -0500

    Updated from Brython Server: 2/4/2019 2:32:45 PM

--- picture.py
+++ picture.py
@@ -28,7 +28,7 @@ from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, El
 
 # add your code here \/  \/  \/
 
-#red = Color(0xff0000, 1.0)
+red = Color(0xff0000, 1.0)
 orange = Color(0xffa500, 1.0)
 #yellow = Color(0xffff00, 1.0)
 #green = Color(0x00ff00, 1.0)
@@ -37,22 +37,22 @@ orange = Color(0xffa500, 1.0)
 #black = Color(0x000000, 1.0)
 
 #blackline = LineStyle(1, black)
-#redline = LineStyle(1, red)
+redline = LineStyle(1, red)
 orangeline = LineStyle(1, orange)
 #yellowline = LineStyle(1, yellow)
 #greenline = LineStyle(1, green)
 #blueline = LineStyle(1, blue)
 #purpleline = LineStyle(1, purple)
 
-#redrectangle = RectangleAsset(200, 20, redline, red)
+redrectangle = RectangleAsset(200, 20, redline, red)
 orangerectangle = RectangleAsset(200, 20, orangeline, orange)
 #yellowrectangle = RectangleAsset(200, 20, yellowline, yellow)
 #greenrectangle = RectangleAsset(200, 20, greenline, green)
 #bluerectangle = RectangleAsset(200, 20, blueline, blue)
 #purplerectangle = RectangleAsset(200, 20, blueline, blue)
 
-#Sprite(redrectangle, (0, 0)
-Sprite(orangerectangle, (0, 20))
+rain1 = Sprite(redrectangle, (0, 0)
+rain2 = Sprite(orangerectangle, (0, 20))
 #Sprite(yellowrectangle, 0, 40)
 #Sprite(greenrectangle, 0, 60)
 #Sprite(bluerectangle, 0, 80)
